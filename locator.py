0eee4779b874d7e81fe4ad63be5df560599f8a2a
SHIVANK SHARMA <Shivankkumarsingh631997@gmail.com>
2024-02-28 00:17:41 +0530

    msage

--- locator.py
+++ locator.py
@@ -1,5 +1,1 @@
-from selenium.webdriver.common.by import By
-
-class MainPageLocators(object):
-    SEARCH_INPUT=(By.CSS_SELECTOR,'input.cdx-text-input__input')
-    SEARCH_BUTTON  =(By.CSS_SELECTOR,'button.cdx-button.cdx-search-input__end-button')
+print('hello')
